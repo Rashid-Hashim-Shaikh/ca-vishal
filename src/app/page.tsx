9c9e36e960bab04eef60e7cff6c8b0647c0fec97
Rashid-Hashim-Shaikh <contactrashid8@gmail.com>
2025-07-13 09:44:09 +0530

    Remove BackToTop button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       <Services />
       <Contact />
       <Footer />
-      <BackToTop />
+      {/* <BackToTop /> */}
       <WhatsAppFloat />
     </main>
   );
