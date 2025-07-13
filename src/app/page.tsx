a579096fefc733ccc527275b9aca93d005a00c50
Rashid-Hashim-Shaikh <contactrashid8@gmail.com>
2025-07-13 09:52:12 +0530

    Add favicon and fix deployment failure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import About from "@/components/About";
 import Services from "@/components/Services";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
-import BackToTop from "@/components/BackToTop";
+// import BackToTop from "@/components/BackToTop";
 import WhatsAppFloat from "@/components/WhatsAppFloat";
 
 export default function Home() {
